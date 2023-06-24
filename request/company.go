60a4fc53bef9e54dae3d9f00150ff94395fe8dc9
Aclaputra <muhamadacla22@gmail.com>
2023-06-24 20:53:26 +0700

    initial commit

---
+++ request/company.go
@@ -0,0 +1,24 @@
+package request
+
+import validation "github.com/go-ozzo/ozzo-validation"
+
+type (
+	CompanyRequest struct {
+		Name    string `json:"name"`
+		Balance int    `json:"balance"`
+		Address string `json:"address"`
+	}
+
+	TopupCompanyBalance struct {
+		Balance int `json:"balance" validate:"required"`
+	}
+)
+
+// TODO: tuliskan validasi untuk CompanyRequest dengan rule semua field required
+
+func (req TopupCompanyBalance) Validate() error {
+	return validation.ValidateStruct(
+		&req,
+		validation.Field(&req.Balance, validation.Required),
+	)
+}
